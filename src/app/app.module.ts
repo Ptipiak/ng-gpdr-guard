6210e444149aa2419d7c6e4dfe05c37f03e720e9
raphael beck-protoy <raphael.beckprotoy@etudiant.univ-lr.fr>
2020-01-07 06:43:01 +0100

    Adding a buildable library

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GpdrGuardComponent } from 'gpdr-guard'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GpdrGuardComponent
   ],
   imports: [
     BrowserModule,
